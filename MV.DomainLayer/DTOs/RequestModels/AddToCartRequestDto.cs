41f1f9d4b1e75762a991d81d612ba2256e9c89e4
Nguyễn Viết Hoàng <hoangnv10052004@gmail.com>
2026-01-29 22:49:38 +0700

    Validation Request from FE

--- MV.DomainLayer/DTOs/RequestModels/AddToCartRequestDto.cs
+++ MV.DomainLayer/DTOs/RequestModels/AddToCartRequestDto.cs
@@ -1,8 +1,15 @@
-﻿namespace MV.DomainLayer.DTOs.RequestModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MV.DomainLayer.DTOs.RequestModels
 {
     public class AddToCartRequestDto
     {
+        [Required(ErrorMessage = "Product ID is required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Product ID must be greater than 0")]
         public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Quantity is required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be at least 1")]
         public int Quantity { get; set; }
     }
 }
--- MV.DomainLayer/DTOs/RequestModels/UpdateCartItemRequestDto.cs
+++ MV.DomainLayer/DTOs/RequestModels/UpdateCartItemRequestDto.cs
@@ -1,7 +1,11 @@
-﻿namespace MV.DomainLayer.DTOs.RequestModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MV.DomainLayer.DTOs.RequestModels
 {
     public class UpdateCartItemRequestDto
     {
+        [Required(ErrorMessage = "Quantity is required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be at least 1")]
         public int Quantity { get; set; }
     }
 }
