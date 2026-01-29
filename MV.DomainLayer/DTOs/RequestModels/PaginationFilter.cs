5cb36b18318d674e87fa9c0f94df840f1d70bc30
Nguyễn Viết Hoàng <hoangnv10052004@gmail.com>
2026-01-29 15:47:06 +0700

    Hoang has done the task.
    Pagination Pages.

---
+++ MV.DomainLayer/DTOs/RequestModels/PaginationFilter.cs
@@ -0,0 +1,28 @@
+﻿namespace MV.DomainLayer.DTOs.RequestModels
+{
+    public class PaginationFilter
+    {
+        private const int MaxPageSize = 50; // Giới hạn tối đa 50 item/trang
+        private int _pageSize = 10; // Mặc định 10 item/trang
+
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = value > MaxPageSize ? MaxPageSize : value;
+        }
+
+        public PaginationFilter()
+        {
+            PageNumber = 1;
+            PageSize = 10;
+        }
+
+        public PaginationFilter(int pageNumber, int pageSize)
+        {
+            PageNumber = pageNumber < 1 ? 1 : pageNumber;
+            PageSize = pageSize > MaxPageSize ? MaxPageSize : pageSize;
+        }
+    }
+}
---
+++ MV.DomainLayer/DTOs/ResponseModels/PagedResponse.cs
@@ -0,0 +1,25 @@
+﻿namespace MV.DomainLayer.DTOs.ResponseModels
+{
+    public class PagedResponse<T> : ApiResponse<List<T>>
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalRecords { get; set; }
+        public bool HasPreviousPage => PageNumber > 1;
+        public bool HasNextPage => PageNumber < TotalPages;
+
+        public PagedResponse(List<T> data, int pageNumber, int pageSize, int totalRecords)
+        {
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+            TotalRecords = totalRecords;
+            TotalPages = (int)Math.Ceiling(totalRecords / (double)pageSize);
+
+            Data = data;
+            Success = true;
+            Message = "Success";
+            Errors = null;
+        }
+    }
+}
