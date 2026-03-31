4c0f5e83b40d16d7139093e4cf61b443542574a5
Khanh Le <lequockhanh293@gmail.com>
2026-04-01 01:09:44 +0700

    feat(return-dto): add ReturnRequestItemResponse inner class with SerialNumbers

--- MV.DomainLayer/DTOs/ReturnRequest/ReturnRequestResponse.cs
+++ MV.DomainLayer/DTOs/ReturnRequest/ReturnRequestResponse.cs
@@ -1,5 +1,17 @@
 namespace MV.DomainLayer.DTOs.ReturnRequest;
 
+public class ReturnRequestItemResponse
+{
+    public int OrderItemId { get; set; }
+    public int ProductId { get; set; }
+    public string? ProductName { get; set; }
+    public string? ProductSku { get; set; }
+    public string? ProductImageUrl { get; set; }
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+    public List<string> SerialNumbers { get; set; } = new();
+}
+
 public class ReturnRequestResponse
 {
     public int ReturnRequestId { get; set; }
